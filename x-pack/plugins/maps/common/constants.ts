b27c85947f803d1ddc050f078d5b51e6c35d5401
Bill Allen <ballen@spectric.com>
2022-01-27 09:01:45 -0700

    Added missing LAYER_TYPE.DATASHADER constant.

--- x-pack/plugins/maps/common/constants.ts
+++ x-pack/plugins/maps/common/constants.ts
@@ -74,6 +74,7 @@ export enum LAYER_TYPE {
   HEATMAP = 'HEATMAP',
   BLENDED_VECTOR = 'BLENDED_VECTOR',
   TILED_VECTOR = 'TILED_VECTOR', // similar to a regular vector-layer, but it consumes the data as .mvt tilea iso GeoJson. It supports similar ad-hoc configurations like a regular vector layer (E.g. using IVectorStyle), although there is some loss of functionality  e.g. does not support term joining
+  DATASHADER = 'DATASHADER',
 }
 
 export enum SOURCE_TYPES {
